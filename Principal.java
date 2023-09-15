6a6398a092069454dc22634f4d937af8140129b0
Deyker Gomez <143283320+deiker12345@users.noreply.github.com>
2023-09-14 20:44:32 -0500

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,15 @@
+package InicioDeSesionAPP;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Principal {
+    public static void main(String[] args) {
+        Ventana.main(args);
+    }
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
